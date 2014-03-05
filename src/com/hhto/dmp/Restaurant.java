71d8854bc02e9327cee80e7d27e6974965df9225
hmhagberg <hmhagberg@gmail.com>
2014-03-05 19:37:01 +0200

    Reorganized code to be more reusable. Made json parsing more robust.

--- src/com/hhto/dmp/Restaurant.java
+++ src/com/hhto/dmp/Restaurant.java
@@ -7,8 +7,10 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by hmhagberg on 3.3.2014.
@@ -18,60 +20,200 @@ import java.util.Date;
 public class Restaurant {
     private Context context;
     private String id;
+    private HashMap<Calendar, RestaurantMenu> menusOfTheWeek = new HashMap<Calendar, RestaurantMenu>();
 
     public Restaurant(Context context, String id) {
         this.context = context;
         this.id = id;
     }
 
-    public RestaurantMenu getMenu(Date date) {
-        // TODO: Implement
+    public RestaurantMenu getMenu(Calendar calendar) {
+        return menusOfTheWeek.get(calendar);
     }
 
-    RestaurantMenu getMenuFromCache(Date date) {
+    void createMenus() {
         try {
-            File cacheDir = context.getCacheDir();
-            File jsonFile = new File(cacheDir, id);
-            BufferedReader br = new BufferedReader(new FileReader(jsonFile));
-            StringBuilder jsonString = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null)) {
-                jsonString.append(line);
-                jsonString.append("\n");
+            JSONObject json = buildJsonFromCache();
+            if (jsonIsUpToDate(json)) {
+                buildMenusFromJson(json);
+            } else {
+                downloadMenus();    // TODO: Implement
             }
-            JSONObject json = new JSONObject(new JSONTokener(jsonString.toString()));
-
-            JSONArray courses = json.getJSONArray("courses");
-            JSONObject course;
-            int i = 0;
-            while ((course = courses.getJSONObject(i)) != null) {
-                i++;
-                // TODO: Parse course
-            }
-
-            // TODO: Handle errors properly
+        } catch (JSONException e) { // TODO: Handle errors properly
+            System.out.println(e.getMessage());
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        } catch (JSONException e) {
+        } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    class RestaurantMenu {
-        private final Restaurant restaurant;
-        private final Date date;
-        private final ArrayList<Course> courses;
 
-        RestaurantMenu(Restaurant restaurant, Date date, ArrayList<Course> courses) {
-            this.restaurant = restaurant;
-            this.date = date;
-            this.courses = courses;
+    /**
+     * Check JSONObject holds menu data for current week.
+     * @param json JSONObject to be inspected.
+     * @return True if object is up-to-date, false otherwise.
+     * @throws JSONException
+     */
+    boolean jsonIsUpToDate(JSONObject json) throws JSONException{
+        Calendar today = Calendar.getInstance();
+        int week = json.getJSONObject("meta").getInt("week");
+        return today.WEEK_OF_YEAR == week;
+    }
+
+
+    /**
+     * Build a JSONObject from cached data.
+     * @return JSONObject built.
+     * @throws IOException
+     * @throws JSONException
+     */
+    JSONObject buildJsonFromCache() throws IOException, JSONException{
+        File cacheDir = context.getCacheDir();
+        File jsonFile = new File(cacheDir, id);
+        BufferedReader br = new BufferedReader(new FileReader(jsonFile));
+        StringBuilder jsonString = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            jsonString.append(line);
+            jsonString.append("\n");
+        }
+        JSONObject json = new JSONObject(new JSONTokener(jsonString.toString()));
+        return json;
+    }
+
+    /**
+     * Build RestaurantMenu objects from JSON and insert them to menusOfTheWeek.
+     * @param json JSONObject created from cached or downloaded data.
+     * @throws ParseException thrown when date in JSON is invalid.
+     * @throws JSONException thrown when parsing JSON fails.
+     */
+    void buildMenusFromJson(JSONObject json) throws ParseException, JSONException{
+
+        JSONObject menus = json.getJSONObject("menus");
+        Iterator menusIterator = menus.keys();
+        JSONArray menuArray;
+        JSONObject jsonCourse;
+        int i;
+
+        while (menusIterator.hasNext()) {   // Iterate "whole week"
+            i = 0;
+            String calString = (String) menusIterator.next();
+            Calendar menuCal = parseCalendar(calString);
+            menuArray = menus.getJSONArray(calString);
+            RestaurantMenu menu = new RestaurantMenu(this, menuCal);
+            while ((jsonCourse = menuArray.getJSONObject(i)) != null) { // Iterate courses of a single day
+                Course course = new Course(jsonCourse.getString("title_fi"), jsonCourse.getString("title_en"));
+                course.setProperties(jsonCourse.getString("properties"));
+                menu.addCourse(course);
+                i++;
+            }
+            menusOfTheWeek.put(menuCal, menu);
         }
     }
 
-    class Course {
+    /**
+     * Parse String to Calendar. Required format is yyyy-MM-dd.
+     * @param calString String to be parsed.
+     * @return Resulting Calendar.
+     * @throws ParseException
+     */
+    static Calendar parseCalendar(String calString) throws ParseException{
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = formatter.parse(calString);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    }
 
+
+    /**
+     * Menu of a particular restaurant for a single day.
+     */
+    class RestaurantMenu {
+        private Restaurant restaurant;
+        private Calendar calendar;
+        private ArrayList<Course> courses = new ArrayList<Course>();
+
+        RestaurantMenu(Restaurant restaurant, Calendar calendar) {
+            this.restaurant = restaurant;
+            this.calendar = calendar;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder collector = new StringBuilder();
+            for (Course course: courses) {
+                collector.append(course.toString());
+                collector.append("\n");
+            }
+            return collector.toString();
+        }
+
+        public void addCourse(Course course) {
+            courses.add(course);
+        }
+
+        public ArrayList<Course> getCourses() {
+            return courses;
+        }
+    }
+
+
+    /**
+     * A single course of a menu.
+     */
+    class Course {
+        private final String titleFi;
+        private final String titleEn;
+        private boolean glutenFree = false;     // Property "G"
+        private boolean lactoseFree = false;    // Property "L"
+        private boolean milkFree = false;       // Property "M"
+
+        Course(String titleFi, String titleEn) {
+            this.titleFi = titleFi;
+            this.titleEn = titleEn;
+        }
+
+        @Override
+        public String toString() {
+            return titleFi;
+        }
+
+        /**
+         * Set properties for course. Meaning of properties is as follows:
+         * "G" = gluten-free
+         * "L" = lactose-free
+         * "M" = milk-free
+         * @param properties: A single String or an array of Strings containing
+         *                  properties for course.
+         */
+        public void setProperties(String... properties) {
+            for (String property: properties) {
+                if (property.contains("G")) {
+                    glutenFree = true;
+                }
+                if (property.contains("L") && !property.contains("VL")) {    // Don't label low-lactose as lactose free
+                    lactoseFree = true;
+                }
+                if (property.contains("M")) {
+                    milkFree = true;
+                }
+            }
+        }
+
+        public boolean isGlutenFree() {
+            return glutenFree;
+        }
+
+        public boolean isLactoseFree() {
+            return lactoseFree;
+        }
+
+        public boolean isMilkFree() {
+            return milkFree;
+        }
     }
 }
