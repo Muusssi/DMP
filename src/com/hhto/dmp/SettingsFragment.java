f88c911e9dd9302e0f7f9308a1e846560ca16440
hmhagberg <hmhagberg@gmail.com>
2014-03-11 14:20:45 +0200

    Created settings.

---
+++ src/com/hhto/dmp/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.hhto.dmp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by hmhagberg on 11.3.2014.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
+
+
