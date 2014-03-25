bc2075e7536f1b07d1ff7f5b68556f1b674ec12d
Muusssi <tommi.oinonen@gmail.com>
2014-03-25 20:25:36 +0200

    SodexoRestaurant.java was missing on last commit

--- src/com/hhto/dmp/SodexoRestaurant.java
+++ src/com/hhto/dmp/SodexoRestaurant.java
@@ -5,6 +5,13 @@ import android.content.Context;
 import android.os.AsyncTask;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.MalformedURLException;
+
 
 /**
  * Created by hmhagberg on 8.3.2014.
@@ -13,33 +20,69 @@ import org.json.JSONObject;
 public class SodexoRestaurant extends Restaurant {
 
     public SodexoRestaurant(Context context) {
-        super(context, "Sodexo", "Sodexo");
-        // downloader = new Downloader();
+        super(context, "Sodexo", "141");
+
     }
 
     @Override
     void downloadData() {
-        // downloader.execute(this);
+        Downloader downloader = new Downloader();
+
+        downloader.execute(this);
+        /*
         try {
-            String testJson = "{\"meta\":{\"week\":12},\"menus\":{\"2014-03-17\":[{\"title_fi\":\"Vettä ja leipää\",\"title_en\":\"Meat kaemae\",\"properties\":\"L, G\",\"desc_fi\":\"\",\"desc_en\":\"\",\"desc_se\":\"\"},{\"title_fi\":\"Munia ja pekonia\",\"title_en\":\"Eggs and bacon\",\"properties\":\"\",\"desc_fi\":\"\",\"desc_en\":\"\",\"desc_se\":\"\"}]}}";
+            String testJson = "{\"meta\":{\"week\":13},\"menus\":{\"2014-03-25\":[{\"title_fi\":\"Vettä ja leipää\",\"title_en\":\"Meat kaemae\",\"properties\":\"L, G\",\"desc_fi\":\"\",\"desc_en\":\"\",\"desc_se\":\"\"},{\"title_fi\":\"Munia ja pekonia\",\"title_en\":\"Eggs and bacon\",\"properties\":\"\",\"desc_fi\":\"\",\"desc_en\":\"\",\"desc_se\":\"\"}]}}";
             JSONObject json = new JSONObject(testJson);
             menusOfTheWeek = buildMenuMap(json);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        */
     }
 
     class Downloader extends AsyncTask<SodexoRestaurant, Void, String> {
 
+        
         @Override
         protected String doInBackground(SodexoRestaurant... params) {
-            // TODO: Write HTTP GET request
+            //Downloads Json from www.sodexo.fi
+            try {
+                String url = "http://www.sodexo.fi/ruokalistat/output/daily_json/"+params[0].getId()+"/2014/03/26/fi";
+                URL urlObj = new URL(url);
+
+                HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+
+                //Read the response
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                //Response Json String
+                String s = response.toString();
+
+                return s;
+
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
             return null;
         }
 
         @Override
         protected void onPostExecute(String s) {
-            // TODO: Parse downloaded JSON
+            // TODO Parse downloaded JSON
+
+
         }
     }
 }
